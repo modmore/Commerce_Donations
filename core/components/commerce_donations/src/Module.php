c7180bb133aeee889fce39441ce94fe934907957
Mark Hamstra <hello@markhamstra.com>
2023-03-08 00:36:16 +0100

    Process donation when order is complete

--- core/components/commerce_donations/src/Module.php
+++ core/components/commerce_donations/src/Module.php
@@ -1,6 +1,9 @@
 <?php
 namespace modmore\Commerce_Donations;
 
+use comDonation;
+use comDonationCause;
+use comDonationProduct;
 use Commerce;
 use modmore\Commerce\Admin\Configuration\About\ComposerPackages;
 use modmore\Commerce\Admin\Sections\SimpleSection;
@@ -8,6 +11,7 @@ use modmore\Commerce\Dispatcher\EventDispatcher;
 use modmore\Commerce\Events\Admin\GeneratorEvent;
 use modmore\Commerce\Events\Admin\PageEvent;
 use modmore\Commerce\Events\Admin\TopNavMenu;
+use modmore\Commerce\Events\OrderState;
 use modmore\Commerce\Modules\BaseModule;
 use modmore\Commerce_Donations\Admin\Causes;
 
@@ -39,11 +43,12 @@ class Module extends BaseModule {
         // Add the xPDO package, so Commerce can detect the derivative classes
         $path = dirname(__DIR__) . '/model/';
         $this->adapter->loadPackage('commerce_donations', $path);
-        $this->adapter->loadClass(\comDonationProduct::class);
+        $this->adapter->loadClass(comDonationCause::class);
+        $this->adapter->loadClass(comDonationProduct::class);
 
         // Add template path to twig
-//        $root = dirname(__DIR__, 2);
-//        $this->commerce->view()->addTemplatesPath($root . '/templates/');
+        $root = dirname(__DIR__);
+        $this->commerce->view()->addTemplatesPath($root . '/templates/');
 
         // Add some custom pages
         $dispatcher->addListener(
@@ -73,6 +78,56 @@ class Module extends BaseModule {
 
 
                 $event->setItems($items);
+            }
+        );
+
+        // When processing the order, save the donation
+        $dispatcher->addListener(
+            Commerce::EVENT_STATE_CART_TO_PROCESSING,
+            function(OrderState $event) {
+                $order = $event->getOrder();
+                $items = $order->getItems();
+
+                foreach ($items as $item) {
+                    if (!$item->getProperty('is_donation')) {
+                        continue;
+                    }
+
+                    /** @var comDonationCause $cause */
+                    $cause = $this->adapter->getObject(comDonationCause::class, [
+                        'id' => (int)$item->getProperty('donation_cause')
+                    ]);
+                    if (!$cause) {
+                        $order->log('Can\'t save donation, cause not found with ID ' . $item->getProperty('donation_cause'));
+                        continue;
+                    }
+
+                    $donation = $this->adapter->newObject(comDonation::class);
+                    $donation->fromArray([
+                        'cause' => (int)$item->getProperty('donation_cause'),
+                        'test' => $order->get('test'),
+                        'order' => $order->get('id'),
+                        'item' => $item->get('id'),
+                        'user' => $order->getUser() ? $order->getUser()->get('id') : 0,
+
+                        'donated_on' => time(),
+                        'currency' => $order->get('currency'),
+                        'amount' => $item->get('total'),
+                        'amount_ex_tax' => $item->get('total_ex_tax'),
+
+                        'donor_public' => $item->getProperty('donor_public', false),
+                        'donor_name' => $item->getProperty('donor_name', ''),
+                        'donor_note' => $item->getProperty('donor_note', ''),
+                    ]);
+
+                    if ($donation->save()) {
+                        $order->log('Processed ' . $item->get('total_formatted') . ' donation for '. $cause->get('name'));
+                        $cause->updateTotals();
+                    }
+                    else {
+                        $order->log('Failed saving ' . $item->get('total_formatted') . ' donation for '. $cause->get('name'));
+                    }
+                }
             }
         );
     }
